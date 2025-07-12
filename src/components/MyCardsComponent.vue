fe11a429a55d09ebc4263d58f6d3ce5ec26729e8
glowingstone124 <hanserofficial@outlook.com>
2025-07-12 16:26:36 +0800

    add card face change

--- src/components/MyCardsComponent.vue
+++ src/components/MyCardsComponent.vue
@@ -1,6 +1,18 @@
-<script setup>
+<script setup lang="ts">
 import {onMounted, ref} from 'vue'
 import axios from "axios";
+
+function apply(cardId: number) {
+	console.log("应用卡片", cardId)
+	axios.post(`https://api.qoriginal.vip/qo/authorization/account/card/custom`,
+		{cardId: cardId}
+		, {
+			headers: {
+				"token": localStorage.getItem("token")
+			}
+		},
+	)
+}
 
 const cards = ref([
 	{
@@ -12,10 +24,10 @@ const cards = ref([
 	}
 ])
 const rarityMap = {
-	1: { name: "普通话", color: "#47c9da" },
-	2: { name: "稀有", color:  "#55CE85" },
-	3: { name: "史诗", color: "#f10d0d" },
-	4: { name: "限定", color:  "#ff9800" }
+	1: {name: "普通话", color: "#47c9da"},
+	2: {name: "稀有", color: "#55CE85"},
+	3: {name: "史诗", color: "#f10d0d"},
+	4: {name: "限定", color: "#ff9800"}
 }
 onMounted(async () => {
 	try {
@@ -44,7 +56,7 @@ onMounted(async () => {
 <template>
 	<div class="gallery">
 		<div class="card" v-for="card in cards" :key="card.id">
-			<img :src="card.file_url" :alt="card.name" />
+			<img :src="card.file_url" :alt="card.name"/>
 			<div class="name-bar">
 				<h3>{{ card.name }}</h3>
 			</div>
@@ -56,6 +68,7 @@ onMounted(async () => {
 				>
 					{{ rarityMap[card.rarity]?.name || "未知" }}
 				</span>
+				<button class="apply" @click="apply(card.id)">应用</button>
 			</div>
 		</div>
 	</div>
@@ -141,5 +154,19 @@ onMounted(async () => {
 	font-weight: bold;
 }
 
+.apply {
+	z-index: 9999;
+	margin-top: 5px;
+	font-size: 0.8rem;
+	border-radius: 6px;
+	border: none;
+	padding: 6px 10px;
+	background-color: #3ca241;
+}
+
+.apply:hover {
+	background-color: #4cb751;
+	cursor: pointer;
+}
 
 </style>
